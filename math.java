353a3852d0a7e017a5347f302db6e09d99849003
PatelDevarsh07 <devutpatel0403@gmail.com>
2022-09-20 08:46:07 -0700

    add file

---
+++ math.java
@@ -0,0 +1,19 @@
+package j4maths;
+
+class maths{
+	private int a=10;
+	private int b=20;
+	
+	public void printdata(){
+	System.out.println("a=>"+a+"b=>"+b);	
+	}
+}
+
+public class math {
+
+	public static void main(String[] args) {
+		math
+
+	}
+
+}
